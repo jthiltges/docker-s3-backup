e22178565ac88b9005e80f38c0b176fd66803d07
John Thiltges <jthiltges2@unl.edu>
2021-07-07 15:03:59 -0500

    Rework the script a bit

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM minio/mc:latest
 
+RUN microdnf install tar xz
 COPY s3-backup.sh /usr/local/bin/
 
 ENTRYPOINT ["/usr/local/bin/s3-backup.sh"]
--- s3-backup.sh
+++ s3-backup.sh
@@ -1,49 +1,62 @@
 #!/bin/bash
 
-ENCRYPTION_KEY=/encryption.key
+ENCRYPTION_KEY=/secret/encryption.key
 
 usage () {
     cat <<EOF
-This tool requires an encryption key in /encryption-key
+This tool requires an encryption key in $ENCRYPTION_KEY
 and the following environment variables
 - S3_ACCESS_KEY
 - S3_BUCKET
 - S3_DEST_DIR
 - S3_SECRET_KEY
-- S3_URL (must start with https://)
+- S3_ENDPOINT (accessed via HTTPS)
 EOF
     exit 1
 }
 
+# Exit on error or if unset variables are expanded
+set -eu
+
+# Configure "dest" alias
+export MC_HOST_dest="https://$S3_ACCESS_KEY:$S3_SECRET_KEY@$S3_ENDPOINT"
+dest="dest/$S3_BUCKET/$S3_DEST_DIR/"
+
+# Generate a key ID (to identify the key used when they are rotated)
+keyid=$(sha256sum $ENCRYPTION_KEY | cut -c 1-5)
+
 # S3_* env vars are required, the url must be properly formatted, and
 # there must be an encryption key
-if [[ -z $S3_ACCESS_KEY$S3_BUCKET$S3_DEST_DIR$S3_SECRET_KEY$S3_URL ]] ||
-       [[ $S3_URL != https://* ]] ||
-       [[ ! -f $ENCRYPTION_KEY ]]; then
+if [[ ! -f $ENCRYPTION_KEY ]]; then
     usage
 fi
 
-# Create an alias for the S3 endpoint/credentials
-# S3_URL must start with "https://"
-mc alias set dest "$S3_URL" "$S3_ACCESS_KEY" "$S3_SECRET_KEY"
+# Show commands
+set -x
 
-
-tmpdir=$(mktemp -d)
-chmod 700 $tmpdir
-backup=$tmpdir/backup-$(date +%Y%m%d-%H%M).tar
-tar -cvf $backup /input/*
+backup=/tmp/backup-$(date +%Y%m%d-%H%M).tar.xz
+outfile="$backup.enc.$keyid"
+tar -cJvf $backup --directory /input .
 
 # Encrypt the tarball contents using a key mounted to
 # $ENCRYPTION_KEY
 gpg --batch \
+    --quiet \
     --passphrase-file $ENCRYPTION_KEY \
-    --output "$backup.enc"
+    --output "$outfile" \
     --symmetric \
     --cipher-algo AES256 \
     $backup
 
+# Decrypting:
+#gpg --batch \
+#    --passphrase-file $ENCRYPTION_KEY \
+#    --decrypt \
+#    "$backup.enc"
+
 # Copy 
 mc cp \
+   --quiet \
    --preserve \
-   $backup
-   "dest/$S3_BUCKET/$S3_DEST_DIR/"
+   "$outfile" \
+   "$dest"
